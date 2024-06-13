9f6ab05224bdbde288fd332bc1b5ccbcf2c99aad
Michael Jeffrey Celestino <emjhay.celstino20@gmail.com>
2024-06-13 21:00:15 +0000

    First CommitSecond Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import App from './pages/App.jsx'
+import LoginPage from './pages/LoginPage.jsx';
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <App/>,
+    errorElement: <div>Error 404 not found</div>
+  },{
+    path:'/LoginPage',
+    element: <LoginPage/>,
+    errorElement: <div>Error 404 not found</div>
+  }
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
+      },
+      dropShadow: {
+        '3xl': '7px 12px 48px -12px rgba(255,0,0,1)',
+        '4xl': [
+            '0 35px 35px rgba(0, 0, 0, 0.25)',
+            '0 45px 65px rgba(0, 0, 0, 0.15)'
+        ]
+      }
+    },
   },
   plugins: [],
 }
